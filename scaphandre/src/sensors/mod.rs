c3e9d048be13123a85b4e1bcda9c16a813f69a6d
merzouka <marzoukayouness@gmail.com>
2026-06-18 18:44:16 +0100

    feat(formula): Changed to (1 + ACTIVE) * APERF for core

--- scaphandre/src/sensors/mod.rs
+++ scaphandre/src/sensors/mod.rs
@@ -543,7 +543,7 @@ impl Topology {
             .iter()
             .map(|c| {
                 if let Some(metrics) = c.get_core_metrics_delta() {
-                  (1_f64 + metrics.active_percentage) * metrics.aperf as f64 * (metrics.aperf as f64 / metrics.mperf as f64)
+                  (1_f64 + metrics.active_percentage) * metrics.aperf as f64
                 } else {
                     0_f64
                 }
